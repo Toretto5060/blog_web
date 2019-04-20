05a095a630f2feba697691e0b260bebe5af2e573
Toretto5060 <877692526@qq.com>
2019-04-20 18:13:44 +0800

    删除

--- src/api/index.js
+++
@@ -1,9 +0,0 @@
-import axiosIns from './resouce.js';
-// let host = 'http://my.toretto.top:1234/'
-let host = 'http://192.168.6.129:1234/'
-
-export const registerUser = params => {return axiosIns.post("" + host + "register", params)}
-export const checkUser = params => {return axiosIns.post("" + host + "checkUser", params)}
-export const loginIn = params => {return axiosIns.post("" + host + "login", params)}
-export const tast = params => { return axiosIns.get("" + host + "tast", params)}
-export const checkLogin = params => { return axiosIns.get("" + host + "checkLogin", params)}
